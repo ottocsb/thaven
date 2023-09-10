6e06db68f6f762485be2cad4a97cc06104814be8
ottocsb <w2606662632@gmail.com>
2023-09-10 11:27:02 +0800

    chore(config): Update project configuration and upgrade dependencies

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,16 @@
 <script lang="ts" setup>
 defineProps<{ msg: string }>()
 
-const visits = useVisits()
-
-const {t} = useI18n()
+const { t } = useI18n()
 </script>
 
 <template>
 	<div class="mt-10 flex flex-col items-center space-y-7">
 		<h1 class="text-4xl">{{ msg }}</h1>
-		<Counter/>
+		<Counter />
 		<p>
 			{{ t('edit') }}
 			<code>components/HelloWorld.vue</code> {{ t('to test HMR') }}
-		</p>
-
-		<p>
-			{{ t('check out') }}
-			<a href="https://github.com/dishait/tov-template" target="_blank">
-				tov-template </a
-			>, {{ t('the official Tov + Vue + Vite template') }}
-		</p>
-		<p class="read-the-docs">
-			{{ t('The total number of views is') }}
-			<span class="text-gray-800" dark="text-gray-300">{{ visits ?? 0 }}</span>
 		</p>
 	</div>
 </template>
