c134abc9de96bdcec5aea1c6a777e4b2e16b34d7
ottocsb <w2606662632@gmail.com>
2023-12-26 11:56:05 +0800

    chore(i18n): Update internationalization (i18n) configuration for multiple languages

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,4 +1,4 @@
-<script lang="ts" setup>
+<script setup lang = "ts">
 import { vOnClickOutside } from '@vueuse/components'
 
 const visiable = ref(false)
@@ -14,32 +14,32 @@ const { availableLocales, locale } = useI18n()
   <div class="relative">
     <div class="inline-flex items-center overflow-hidden rounded-md">
       <button
-        :class="visiable ? 'bg-gray-200 bg-gray-500 dark:bg-gray-500' : ''"
         class="h-full cursor-pointer border-0 bg-white p-2 text-gray-600 hover:bg-gray-200 hover:text-gray-700"
         dark="bg-transparent hover:bg-gray-500"
+        :class="visiable ? 'bg-gray-200 bg-gray-500 dark:bg-gray-500' : ''"
         @click.stop="visiable = !visiable"
       >
         <svg
-          class="h-4 w-4"
-          fill="currentColor"
-          viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
+          class="h-4 w-4"
+          viewBox="0 0 20 20"
+          fill="currentColor"
         >
           <path
-            clip-rule="evenodd"
-            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
             fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd"
           />
         </svg>
       </button>
     </div>
 
-    <Transition mode="out-in" name="fade">
+    <Transition name="fade" mode="out-in">
       <div
         v-if="visiable"
         v-on-click-outside.bubble="dropdownHandler"
-        class="absolute end-0 z-10 mt-2 w-56 rounded-md bg-white shadow-lg divide-y divide-gray-100"
         dark="bg-gray-500"
+        class="absolute end-0 z-10 mt-2 w-56 rounded-md bg-white shadow-lg divide-y divide-gray-100"
       >
         <div class="p-2">
           <span
--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,20 +1,35 @@
+import { defu } from 'defu'
 import { createI18n } from 'vue-i18n'
 
-const messages = Object.fromEntries(
-  Object.entries(
-    import.meta.glob('../../locales/*.y(a)?ml', {
-      eager: true,
-    }),
-  ).map(([key, value]) => {
-    const yaml = key.endsWith('.yaml')
-    return [key.slice(14, yaml ? -5 : -4), value.default]
-  }),
-)
+const yamls = import.meta.glob('../../locales/*/**.y(a)?ml', {
+  eager: true,
+})
+
+const languages = Object.entries(yamls).map(([key, value]) => {
+  const yaml = key.endsWith('.yaml')
+  key = key.slice(14, yaml ? -5 : -4)
+  // 如果有子模块，则分割
+  if (key.includes('/'))
+    key = key.split('/')[0]
+
+  return { [key]: value.default }
+})
+
+const messages = defu({}, ...languages)
+
+// localStorage 中的 locale，第二个参数为默认值
+// https://vueuse.org/core/useStorage/#usestorage
+const storageLocale = useStorage('locale', '简体中文')
 
 export const i18n = createI18n({
-  legacy: false,
-  locale: 'English', // 默认语言
   messages,
+  legacy: false,
+  globalInjection: true,
+  allowComposition: true,
 })
+
+// 同步本地 localStorage 和 i18n
+// https://vueuse.org/shared/syncRef/#syncref
+syncRef(storageLocale, i18n.global.locale)
 
 export default i18n
